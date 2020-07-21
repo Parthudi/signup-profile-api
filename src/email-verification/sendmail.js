5b4566cc7ae6018935b3e0bcb79c664f2664b020
Parthudi <65971741+Parthudi@users.noreply.github.com>
2020-07-21 21:56:04 +0530

    email verify

--- src/email-verification/sendmail.js
+++ src/email-verification/sendmail.js
@@ -1,8 +1,10 @@
 const sgmail = require('@sendgrid/mail')
-
+const log4js = require("log4js")
 
 sgmail.setApiKey(process.env.SENDGRID_API_KEY)
 
+var logger = log4js.getLogger()
+logger.level = "debug"
 
 const sendingWelcomeMail = (email, name, random) => {
 
@@ -11,9 +13,9 @@ const sendingWelcomeMail = (email, name, random) => {
         from: 'parmarparth597@gmail.com',
         subject: 'welcome to our application',
         html: '<h2> welcome mr/mrs ' +name+ ' to our app let us know if u have any query regarding our application </h2> <br>' +
-              'OTP: '+random
-                
-        })
+              '<b><h3> OTP:'+random+ '</h3></b>'
+      })
+      logger.debug("Email sent perfectly")
 }
 
 module.exports = {sendingWelcomeMail}
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -29,5 +29,5 @@ const auth = async(req, res, next) => {
 }
 
 module.exports = {
-    auth,
+    auth
 } 
